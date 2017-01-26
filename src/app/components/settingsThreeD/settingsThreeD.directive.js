de29c9081516ecfd06ca9cbee03affdd26d4571e
netanel <netanelm987@gmail.com>
2017-01-26 23:32:25 +0200

    Adding an option to change the size and height of the 3D

--- src/app/components/settingsThreeD/settingsThreeD.directive.js
+++ src/app/components/settingsThreeD/settingsThreeD.directive.js
@@ -1,5 +1,5 @@
 /**
- * Created by נתנאל מנצור on 1/1/2017.
+ * Created by Netanel on 1/1/2017.
  */
 export function SettingsThreeDDirective() {
 
@@ -28,7 +28,7 @@ class SettingsThreeDController {
       if(data.display){
         this.display = true;
         that.size = data.model._model.scale.getValue();
-
+        that.height = this.pointsToCartographicArray([this.model.model.position._value])[0].height;
         var heading = Cesium.Math.toRadians(135);
         var pitch = 0;
         var roll = 0;
@@ -48,39 +48,49 @@ class SettingsThreeDController {
     this.height = '';
     this.size = 0
   }
+  myFunct(keyEvent) {
+    if (keyEvent.which === 13)
+      alert('I am an alert');
+  }
+
 
   select3D(model) {
     console.log()
   }
 
-  heightSetting() {
-  }
-
-  sizeSetting(action) {
-    var testNumber = this.size;
-    this.size = Number(this.size.toString().replace(/\D/g, ''));
-    if (this.size != testNumber) {
-      alert('Please do not put words just a number')
+  pointsToCartographicArray(carteians) {
+    if (!_.isArray(carteians) || !_.isObject(carteians[0]) || !carteians[0].x) {
+      return [];
     }
-    this.model.model._model.scale = action === '+' ? this.size++ : this.size--;
+    return  _.map(carteians, (carteian)=> {
+      let cartographics = Cesium.Cartographic.fromCartesian(carteian);
+      cartographics.longitude = Cesium.Math.toDegrees(cartographics.longitude);
+      cartographics.latitude = Cesium.Math.toDegrees(cartographics.latitude);
+      return cartographics;
+    });
+
   }
 
-  rotateSetting() {
+  heightSetting(keyEvent) {
+    if (keyEvent.which === 13){
+      var testNumber = this.height;
+      this.height = Number(this.height.toString().replace(/\D/g, ''));
+      if (this.height != testNumber) {
+        alert('Please do not put words just a number')
+      }
+      let cartograph = this.pointsToCartographicArray([this.model.model.position._value]);
+      this.model.model.position._value = Cesium.Cartesian3.fromDegrees(cartograph[0].longitude, cartograph[0].latitude,this.height)
+    }
   }
 
-  orbitSetting() {
-  }
-
-  rotateSideTop() {
-    console.log('dsffdssf')
-  }
-
-  rotateSideLeft() {
-  }
-
-  rotateSideRight() {
-  }
-
-  rotateSideBottom() {
+  sizeSetting(keyEvent) {
+    if (keyEvent.which === 13){
+      var testNumber = this.size;
+      this.size = Number(this.size.toString().replace(/\D/g, ''));
+      if (this.size != testNumber) {
+        alert('Please do not put words just a number')
+      }
+      this.model.model._model.scale = this.size;
+    }
   }
 }
